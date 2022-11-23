e78d2d2a3467be57a5b1a88e31c00385b1e873a8
alexandre-cruz-nicolas <alenic@gmail.com>
2022-11-23 18:33:23 -0300

    versao2.2.1

--- src/pages/AddPet/index.js
+++ src/pages/AddPet/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 import axios from 'axios';
